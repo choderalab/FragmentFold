b4a3f067d4dbd4cbec5ca13a9c938bd9fee3e40b
Alexander Matthew Payne <alex.payne@choderalab.org>
2024-04-12 08:45:41 -0400

    update cluster scripts

--- science/2024.04.11_test_ligand_transfer_prep/ligand_transfer_docking.sh
+++ science/2024.04.11_test_ligand_transfer_prep/ligand_transfer_docking.sh
@@ -8,12 +8,14 @@
 #BSUB -eo log_files/zika_screen_ligand_transfer_docking.stderr
 source ~/.bashrc
 conda activate asapdiscovery
+
 asap-docking ligand-transfer-docking \
---structure-dir /data/chodera/paynea/fragment-fold/data/ns2-ns3b-zika-fragment-screen/alphafold-flavivirus-structures \
+--structure-dir /data/chodera/paynea/fragment-fold/data/ns2-ns3b-zika-fragment-screen/alphafold-flavivirus-structures/ \
 --ref-structure-dir /data/chodera/paynea/fragment-fold/data/ns2-ns3b-zika-fragment-screen/fragment-screen-inputs/output \
 --cache-dir /lila/data/chodera/asap-datasets/foldalysis/ns2_ns3b_zika/prepped_complexes \
 --save-to-cache \
 --output-dir /lila/data/chodera/asap-datasets/foldalysis/ns2_ns3b_zika/self_docked_complexes \
 --ref-chain B \
---active-site-chain A
+--active-site-chain A \
+--use-dask
 echo done
